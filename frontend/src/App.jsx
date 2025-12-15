c1d5a5449d03714a09b01a8935b164c5dcda160c
Rohan Poudel <rohzanpoudel@gmail.com>
2025-12-14 18:26:02 -0800

    frontend setup, added daisyui, react-toast, tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,28 @@
-const App = ()=>{
-  return( <div> APP</div>)
+import { Route, Routes } from 'react-router'
+import { toast } from 'react-hot-toast'
 
+import React from 'react'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+
+const App = () => {
+  return (
+    <div data-theme = "forest">
+    <button className="btn">click here</button>
+      <Routes>
+      <Route path = "/" element = {<HomePage/>} />
+      <Route path = "/create" element = {<CreatePage/>} />
+      <Route path = "/note/:id" element = {<NoteDetailPage/>} />
+
+
+      </Routes>
+
+
+
+
+    </div>
+  )
 }
+
 export default App
