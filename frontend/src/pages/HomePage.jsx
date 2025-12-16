45f37d221416fad8569f477bdec35028ff5f5209
Rohan Poudel <rohzanpoudel@gmail.com>
2025-12-16 13:20:59 -0800

    added a new component if there are no notes in database

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from '../components/RateLimitedUI'
 import { useEffect } from 'react'
 import api from '../lib/axios'
 import NoteCard from '../components/NoteCard'
+import NotesNotFound from '../components/NotesNotFound'
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false)
@@ -39,6 +40,7 @@ const HomePage = () => {
         {loading && (
           <div className="text-center text-primary py-10"> Loading notes</div>
         )}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
